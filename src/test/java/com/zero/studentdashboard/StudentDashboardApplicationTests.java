81cddaf475e214c3bfbd736adb679d410587e0a4
Abdullah Akbarov <abdullah.samarqandiy@gmail.com>
2023-11-04 20:14:55 +0500

    Initial commit

---
+++ src/test/java/com/zero/studentdashboard/StudentDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.studentdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
